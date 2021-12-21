3645364aa2fcbb5227aa060adcfa80fbf1e75dee
Alexey Panchenko <j66k@yandex.ru>
2021-12-21 18:23:46 +0300

    api/3

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,4 @@
 from rest_framework import routers
-
 from django.urls import include, path
 from rest_framework.authtoken import views
 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-
 from django.shortcuts import get_object_or_404
 
 from posts.models import Comment, Group, Post
